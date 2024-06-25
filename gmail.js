c7f3dc6d53c1d63ebb121efa3470e89c23671101
stktyagi <142912014+stktyagi@users.noreply.github.com>
2024-06-25 21:54:46 +0530

    Add files via upload

---
+++ gmail.js
@@ -0,0 +1,46 @@
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+require('dotenv').config();
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  'http://localhost:5000/oauth2callback'
+);
+
+const generateAuthUrl = () => {
+  return oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+};
+
+const getToken = async (code) => {
+  const { tokens } = await oauth2Client.getToken(code);
+  return tokens;
+};
+
+const getLinkedInEmails = async (tokens) => {
+  oauth2Client.setCredentials(tokens);
+  const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+  const res = await gmail.users.messages.list({
+    userId: 'me',
+    q: 'from:linkedin.com',
+  });
+
+  const messages = res.data.messages || [];
+  const emails = [];
+
+  for (const message of messages) {
+    const msg = await gmail.users.messages.get({
+      userId: 'me',
+      id: message.id,
+    });
+    emails.push(msg.data.snippet);
+  }
+
+  return emails;
+};
+
+module.exports = { generateAuthUrl, getToken, getLinkedInEmails };
